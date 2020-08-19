d5f36c417ac03826aefc033d48da859883267374
Alexander Magana <alexander.jm28@gmail.com>
2020-08-19 17:41:36 -0500

    browse page now shows all books in db

--- frontend/src/browse.js
+++ frontend/src/browse.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './App.css';
-import {Layout, Menu} from 'antd';
+import {Layout, Menu, Table} from 'antd';
+import axios from 'axios';
 
 const {Header, Content, Footer} = Layout;
 
 const Browse = () => {
+    const [state, setstate] = useState([]);
+    const [loading, setloading] = useState(true);
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        await axios.get("http://localhost:8000/api/book/").then(
+            res => {
+                setloading(false);
+                setstate(
+                    res.data.map(row => ({
+                        Title: row.title,
+                        Author: row.author,
+                        id: row.bookID
+                    }))
+                );
+            }
+        );
+    };
+    
+    const columns = [
+        {
+            title: "Title",
+            dataIndex: "Title",
+            width: 150
+        },
+        {
+            title: "Author",
+            dataIndex: "Author",
+            width: 150
+        }
+    ];
+
     return (
         <div className="App">
             <Layout>
@@ -17,10 +52,21 @@ const Browse = () => {
                     </Menu>
                 </Header>
                 <Content>
-                    <h2>Browse Books Here.</h2>
+                    <div>
+                        {loading ? (
+                            "Loading"
+                        ) : (
+                            <Table
+                            columns={columns}
+                            dataSource={state}
+                            pagination={{ pageSize: 50 }}
+                            scroll={{ y: 240 }}
+                            />
+                        )}
+                    </div>
                 </Content>
                 <Footer>
-
+    
                 </Footer>
             </Layout>
         </div>
